3824a931b8dcd09b33793c5e662434fed8e3ca8c
Mathijs Vogelzang <mathijs@gmail.com>
2024-09-02 17:33:53 +0200

    Show top coins on base sepolia on main page

--- web/src/lib/pages/Main.svelte
+++ web/src/lib/pages/Main.svelte
@@ -1,4 +1,15 @@
 <script lang="ts">
+  import UnknownCoin from '../../assets/unknown-coin.svg';
+  import { goto } from '$app/navigation';
+  const coins = (async () => {
+    const response = await fetch('https://coins.vogelcode.com/api/v1/coin/base-sepolia');
+    const coins = await response.json();
+    return coins.map(coin => ({
+      ...coin,
+      icon: coin.icon ?? UnknownCoin,
+      description: coin.description ?? 'No description (yet)'
+    }));
+  })();
 </script>
 
 <svelte:head>
@@ -10,7 +21,42 @@
         <Graphics /> -->
   <h2>Welcome to Coinpedia</h2>
 
-  Here you can register your coins.
-
-  <a href="/coin/0x8D1543277bf8a7038C9596029758fA04719d991C">token1</a>
+  <h3>Top coins on Base Sepolia</h3>
+  {#await coins}
+    <p>loading...</p>
+  {:then coins}
+    <table class="table table-striped table-hover">
+      <tbody>
+        {#each coins as coin}
+          <tr on:click={() => goto(`/coin/${coin.address}`)}>
+            <td class="icon-col"
+              ><img src={UnknownCoin} alt="icon for {coin.symbol}" class="coin-icon" /></td>
+            <td class="symbol-col"
+              ><a href="/coin/{coin.address}"><code>{coin.symbol}</code></a>
+            </td><td><a href="/coin/{coin.address}">{coin.name}</a></td>
+            <td>{coin.description}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/await}
 </div>
+
+<style>
+  .symbol-col {
+    max-width: 4ch;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .coin-icon {
+    width: 2rem;
+    height: 2rem;
+    margin-right: 0.5rem;
+  }
+  .icon-col {
+    width: 3rem;
+    text-align: center;
+  }
+</style>
